b6f1ed84ef37077a4460bb0a1164a49fc377d9f3
R <shreyaspawar96@gmail.com>
2023-09-08 07:33:45 +0000

    Create 4 files

---
+++ src/Components/Expenses/ExpenseForm.js
@@ -0,0 +1,21 @@
+import "./ExpenseForm.css"
+import {useState} from 'react';
+const ExpenseForm = () => {
+
+return (
+    <form>
+<div className='new-expense__controls'>
+<div className='new-expense__control'>
+<label> Title </label> 
+    <input type='text> 
+
+</div> 
+
+
+
+    </form>
+</div>
+);
+}
+
+export default ExpenseForm;
